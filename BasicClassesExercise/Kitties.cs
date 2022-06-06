767dd50e757cd5cf5e5b2ae02dc7eafec9283cab
John Tuck <j.tuck1336@gmail.com>
2022-06-06 15:52:51 -0400

    Exercise completed

---
+++ BasicClassesExercise/Kitties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExercise
+{
+    internal class Kitties
+    {
+        public string Name { get; set; }
+        public string Origin { get; set; }
+        public int Speed { get; set; }
+    }
+}
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var johnsC = new Car();           
+            johnsC.Make = "Honda";
+            johnsC.Model = "Ridgeline";
+            johnsC.Year = 2011;
+
+            Console.WriteLine(johnsC.Make);
+            Console.WriteLine(johnsC.Model);
+            Console.WriteLine(johnsC.Year);
+
+            var johnsK = new Kitties();
+            johnsK.Name = "Tiger";
+            johnsK.Origin = "Africa";
+            johnsK.Speed = 50;
+
+            Console.WriteLine(johnsK.Name);
+            Console.WriteLine(johnsK.Origin);
+            Console.WriteLine(johnsK.Speed);
+
+
+
+
+
+
+        }
+    }
+}
